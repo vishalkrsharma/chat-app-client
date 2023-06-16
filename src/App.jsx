56d1fd32fc3210db9114a0ed79210ebd542030e1
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-06-16 14:00:31 +0530

    axios credentials remove

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { UserContextProvider } from './context/UserContext';
 
 function App() {
   axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
 
   console.log(import.meta.env.VITE_SERVER_URL);
   return (

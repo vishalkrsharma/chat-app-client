2ee8edaa757a4782ee5713cecced369c924862ce
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-06-16 13:55:21 +0530

    axios url changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Routes from './Routes';
 import { UserContextProvider } from './context/UserContext';
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:5000';
+  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
   axios.defaults.withCredentials = true;
 
-  axios.defaults.withCredentials = true;
+  console.log(import.meta.env.VITE_SERVER_URL);
   return (
     <UserContextProvider>
       <Routes />
